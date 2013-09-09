81c460e55eefdffa16b54a61f969cad21fec6b2c
Sanjay Yermalkar <syermalk@searshc.com>
2013-09-08 20:07:50 -0500

    Made the code concise for the blog

--- src/test/java/pig/WordCountPigTest.java
+++ src/test/java/pig/WordCountPigTest.java
@@ -7,31 +7,20 @@ import org.junit.Test;
 import java.io.IOException;
 
 /**
- * Created with IntelliJ IDEA.
- * User: syermalk
- * Date: 10/24/12
- * Time: 9:50 PM
- * To change this template use File | Settings | File Templates.
+ * User: MetaScale
  */
 public class WordCountPigTest {
 
     @Test
     public void testWordCount() throws IOException, ParseException {
-        String[] args = {
-
-        };
-
-        PigTest test = new PigTest("./src/main/pig/wordcount.pig", args);
-
+        PigTest test = new PigTest("./src/main/pig/wordcount.pig", new String[]{});
         String[] input = {
                 "hello hello hello",
                 "hello hello hello"
         };
-
         String[] output = {
                 "(6,hello)"
         };
-
         test.assertOutput("A", input, "D", output);
     }
 }
